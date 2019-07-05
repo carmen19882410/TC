3e1ad148f99e34a3bf731c720cd334b8421d91b3
carmen19882410 <52342843+carmen19882410@users.noreply.github.com>
2019-07-05 20:06:54 +0900

    upload

---
+++ import_swc3.py
@@ -0,0 +1,104 @@
+
+#%%
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 20:33:29 2019
+
+@author: René, Adna and Carmen
+"""
+import os
+wdir=os.getcwd()
+
+from netpyne.support import morphology
+from netpyne import sim
+from neuron import h
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import platform
+import neuron as nrn
+if platform.system() == 'Windows':
+    nrn.load_mechanisms(os.path.join(wdir + 'Morph_practice', 'modfiles'))
+    import sys
+    sys.path.append('C:', 'nrn', 'lib', 'python')
+    sys.path.append('D:', 'Okinawa', 'Python')
+    sys.path.append(wdir + 'Morph_practice', 'modfiles')
+
+#load cell as mycell
+#    def getmorph(self):
+myCell= morphology.Cell()
+morphology.load(filename=os.path.join(wdir,'SWC', 'MN_morphology.swc'), cell=myCell)
+
+#plot loaded cell
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+morphology.shapeplot(h, ax)
+           
+#get the sections from the cell
+secs=list(h.allsec());
+secs_all = secs
+soma = []
+axon = []
+dend = []
+
+
+    #get the sections fro soma, axon and dend
+    #def getset(self):
+for sec in secs:
+    name = sec.name()   
+    if name[0:4] == 'soma':
+        soma.append(sec)
+    if name[0:4] == 'axon':
+        axon.append(sec)
+    if name[0:4] == 'dend':
+        dend.append(sec)
+
+
+
+
+
+
+
+class TC_cell():
+
+    def __init__(self):
+            
+            self.add_biophys_all()
+            self.add_biophys_axon()
+            self.add_biophys_soma()
+            self.add_biophys_dend()
+            #self.getmorph()
+            #self.getset()
+    
+    def add_biophys_all(self):
+        for sec in secs_all:
+            sec.Ra = 178 #add a axial resistance of 100ohm per square cm
+            sec.cm = 0.8 # Membrane capacitance in micro Farads / cm^2
+
+    #give the cell biphys props
+    def add_biophys_soma(self):       
+        for sec in soma:
+            sec.insert('hh')
+            #sec.insert('na')
+            #sec.insert('kv')
+            
+    
+    def add_biophys_axon(self):   
+        for sec in axon:
+            sec.insert('hh')
+            #sec.insert('na')
+            #sec.insert('kv')
+        
+    def add_biophys_dend(self):       
+        for sec in dend:
+            sec.insert('pas')
+        
+    add_biophys_all(secs_all)     
+    add_biophys_soma(soma)
+    add_biophys_axon(axon)
+    add_biophys_dend(dend)
+
+
+def MakeCell():
+    TC = TC_cell()
+    return TC
---
+++ netParams.py
@@ -0,0 +1,82 @@
+from netpyne import specs, sim
+
+try:
+	from __main__ import cfg  # import SimConfig object with params from parent module
+except:
+	from cfg import cfg  # if no simConfig in parent module, import directly from tut8_cfg module
+
+# Network parameters
+netParams = specs.NetParams()  # object of class NetParams to store the network parameters
+
+
+################################################################################################
+## Population parameters
+################################################################################################
+
+
+#netParams.popParams['S'] = {'cellType': 'PYR', 'numCells': 20, 'cellModel': 'HH'}
+#netParams.popParams['M'] = {'cellType': 'PYR', 'numCells': 20, 'cellModel': 'HH'}
+
+netParams.popParams['CN'] = {'cellType': 'CN', 'numCells': 1, 'cellModel': 'HH'}
+netParams.popParams['TC'] = {'cellType': 'TC_cell', 'numCells': 1, 'cellModel': 'HH'}
+
+#netParams.popParams['CA_229hoc'] = {'cellType': 'DET', 'numCells': 1, 'cellModel': 'blank'}
+
+
+
+################################################################################################
+## Cell property rules
+################################################################################################
+
+cellRule = netParams.importCellParams(label = 'CN', conds = {'pop': 'CN'} , fileName = 'import_swc3.py', cellName = 'MakeCell', importSynMechs=True)
+cellRule = netParams.importCellParams(label = 'TC_cell', conds = {'pop': 'TC'} , fileName = 'import_swc3.py', cellName = 'MakeCell', importSynMechs=True)
+#cellRule = netParams.importCellParams(label = 'CA_229hoc', conds = {'pop': 'CA_229hoc'} , fileName = 'cells/CA_229.hoc', cellName = '', importSynMechs=False)
+
+
+#cellRule = {'conds': {'cellType': 'PYR'},  'secs': {}} 	# cell rule dict, make an new matrix called secs, in which the data will be spit
+#cellRule['secs']['soma'] = {'geom': {}, 'mechs': {}}  														# soma params dict
+#cellRule['secs']['soma']['geom'] = {'diam': 18.8, 'L': 18.8, 'Ra': 123.0}  			# big dictionary with all the values we want to add into the model						# soma geometry
+#cellRule['secs']['soma']['mechs']['hh'] = {'gnabar': 0.12, 'gkbar': 0.036, 'gl': 0.003, 'el': -70}  		# soma hh mechanism
+#netParams.cellParams['PYRrule'] = cellRule  
+												# add dict to list of cell params
+
+
+
+################################################################################################
+# Stimulation parameters
+################################################################################################
+    
+#netParams.stimSourceParams['bkg'] = {'type': 'NetStim', 'rate': 10, 'noise': 0.5}#intrinsic noise is also included
+#netParams.stimTargetParams['bkg->PYR'] = {'source': 'bkg', 'conds': {'cellType': 'PYR'}, 'weight': 0.01, 'delay': 5, 'synMech': 'exc'}
+
+netParams.stimSourceParams['CB_syn'] = {'type': 'AlphaSynapse', 'onset': 'uniform(300,600)', 'tau': 5, 'gmax': 'post_ynorm', 'e': 0}
+netParams.stimTargetParams['CB_syn->TC'] = {'source': 'CB_syn', 'sec':'soma', 'loc': 0.5, 'conds': {'pop':'TC'}}
+
+netParams.stimSourceParams['pulse'] = {'type': 'IClamp', 'del':200, 'dur':10, 'amp':0.4} #ms  nA
+netParams.stimTargetParams['pulse->TC'] = {'source': 'pulse', 'conds': {'cellType': 'TC_cell'}, 'sec':'soma_0', 'loc':0.5}
+
+
+
+################################################################################################
+## Synaptic mechanism parameters, most of them get imported from somewhere else
+################################################################################################
+
+netParams.synMechParams['exc'] = {'mod': 'Exp2Syn', 'tau1': 0.1, 'tau2': cfg.synMechTau2, 'e': 0}  # excitatory synaptic mechanism
+#netParams.synMechParams['inh'] = {'mod': 'Exp2Syn', 'tau1': 0.6, 'tau2': 8.5, 'e': -75}  # GABA synaptic mechanism
+
+
+
+################################################################################################
+## Cell connectivity rules
+################################################################################################
+
+netParams.connParams['CN->TC'] = { 	#  S -> M label
+	'preConds': {'pop': 'CN'}, 	# conditions of presyn cells
+	'postConds': {'pop': 'TC'}, # conditions of postsyn cells
+	'probability': 1, 			# probability of connection
+	'weight': cfg.connWeight, 		# synaptic weight
+	'delay': 5,						# transmission delay (ms)
+	'synMech': 'exc'}   			# synaptic mechanism
+
+
+
